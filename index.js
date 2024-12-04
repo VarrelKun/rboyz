3bd758554dcce86bf05871be7ffe4d5627d9ed42
v a r r <91899516+VarrelKun@users.noreply.github.com>
2024-12-04 13:11:59 +0700

    Add files via upload
    Signed-off-by: v a r r <91899516+VarrelKun@users.noreply.github.com>

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -41,7 +41,7 @@ const upload = multer({ storage });
 // Routes
 app.get('/', async (req, res) => {
     const photos = await Photo.find();
-    res.render('index', { photos });
+    res.render('gallery', { photos });
 });
 
 app.get('/upload', (req, res) => {

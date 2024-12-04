1beb97d1aebb937a0e787c53c81e539fc4709249
v a r r <91899516+VarrelKun@users.noreply.github.com>
2024-12-04 12:58:49 +0700

    Update index.js
    Signed-off-by: v a r r <91899516+VarrelKun@users.noreply.github.com>

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
